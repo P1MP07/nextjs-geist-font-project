bd931a7aba36dc5f94dc6faf0c32a1de37aaf412
Piko07 <139527689+P1MP07@users.noreply.github.com>
2025-05-30 20:25:16 -0300

    Add .next/static/chunks/src_app_(authenticated)_financeiro_page_tsx_0c9f42d2._.js 1748647515259

---
+++ .next/static/chunks/src_app_(authenticated)_financeiro_page_tsx_0c9f42d2._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_643d7c8d._.js",
+  "static/chunks/node_modules_axios_lib_99999129._.js",
+  "static/chunks/node_modules_lodash_94b1fd64._.js",
+  "static/chunks/node_modules_recharts_es6_57eb4db6._.js",
+  "static/chunks/node_modules_4f97e309._.js"
+],
+    source: "dynamic"
+});
